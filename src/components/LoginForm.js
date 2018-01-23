4f3b3663c962a2ecddb39697c771afc37c142970
Harish Subramanian <hsubramanian@gohealth.com>
2018-01-22 23:09:48 -0600

    initial integration with firebase.. login works

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
-import {View, TextInput} from 'react-native';
+import {Text} from 'react-native';
 import Button from './common/Button';
 import Card from './common/Card'
 import CardSection from './common/CardSection';
 import {Input} from './common';
-
+import firebase from 'firebase';
 
 class LoginForm extends Component {
 
-    state = {email: '', password:''};
+    state = {email: '', password: '', error: ''};
+
+    onButtonPress() {
+
+        const {email, password} = this.state;
+
+        this.setState({error:''});
+
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
+
+            this.setState({error: " Authentication failed"});
+            // firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
+            //
+            //     this.setState({error: " User creation failed"});
+            //
+            // });
+        })
+
+
+    }
+
 
     render() {
         return (
@@ -31,8 +51,9 @@ class LoginForm extends Component {
 
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>{this.state.error}</Text>
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Log In
                     </Button>
                 </CardSection>
@@ -43,4 +64,14 @@ class LoginForm extends Component {
 
 }
 
+const styles = {
+
+    errorTextStyle: {
+        fontSize: 20,
+        color: 'red',
+        alignSelf: 'center'
+
+    }
+
+};
 export default LoginForm;
